b47e8b0857ae3d7b11b2ef86462f8f4c46ca93c4
John Muthomi <michenijohn20@gmail.com>
2026-08-20 08:59:17 +0300

    Added user class

---
+++ models.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+
+from extensions import db
+
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    searches = db.relationship("SearchHistory", backref="user", lazy=True)
