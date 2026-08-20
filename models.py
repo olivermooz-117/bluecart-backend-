08c435d14c919fd27509de303364527783322089
John Muthomi <michenijohn20@gmail.com>
2026-08-20 09:16:42 +0300

    Fixed broken ref

--- models.py
+++ models.py
@@ -10,5 +10,3 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(255), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
-
-    searches = db.relationship("SearchHistory", backref="user", lazy=True)
