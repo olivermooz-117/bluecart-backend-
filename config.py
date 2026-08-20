3f31b0b15828b713680c1e1060292cdf3eaf4db4
John Muthomi <michenijohn20@gmail.com>
2026-08-20 10:22:00 +0300

    Add Flask app factory and config

---
+++ config.py
@@ -0,0 +1,14 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class Config:
+    SECRET_KEY = os.environ.get("SECRET_KEY", "dev")
+    JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY", "dev-jwt")
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        "DATABASE_URL", "postgresql://postgres:postgres@localhost:5432/bluecart"
+    )
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
